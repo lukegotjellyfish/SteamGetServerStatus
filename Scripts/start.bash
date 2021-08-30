48e20dbfdef0833d3826b7ae621009d8d92cdfd2
Luke Roper <lukegotjellyfish@gmail.com>
2021-08-30 03:17:11 +0100

    sync

--- Scripts/start.bash
+++ Scripts/start.bash
@@ -5,5 +5,8 @@ do
 	python3 FilterResponse.py
 	pwsh -executionpolicy remotesigned -File "Query_US5.ps1"
 	python3 FilterResponse.py
+	
+	gdrive update 1aJhDTFRpzDCkzThP5qX0fxThnQ5AUr7F ServerWatch.csv
+	
 	sleep 300
 done
