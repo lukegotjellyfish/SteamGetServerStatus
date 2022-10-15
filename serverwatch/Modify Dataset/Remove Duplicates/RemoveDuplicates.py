427b8a97886c3b4710bb5240d448c1b3f110d893
Luke <Lukegotjellyfish@gmail.com>
2022-10-15 14:01:51 +0100

    Change export file to same as input

--- serverwatch/Modify Dataset/Remove Duplicates/RemoveDuplicates.py
+++ serverwatch/Modify Dataset/Remove Duplicates/RemoveDuplicates.py
@@ -4,6 +4,6 @@ import pandas as pd
 
 df = reduce(
     lambda df_i, df_j: pd.concat([df_i, df_j]).drop_duplicates(subset=["Date", "Time"]),
-    pd.read_csv("Output.csv", chunksize=100000)
+    pd.read_csv("ServerWatch.csv", chunksize=100000)
 )
 df.to_csv("ServerWatch.csv")
